8bb7e67cf4a36ce0a409d7bdc770b8a8b1e3f0da
Lucas Santos <santos.lucas00@hotmail.com>
2017-04-26 10:39:39 -0300

    Criando uma constante para inserir a imagem

--- app/src/main/java/br/com/fiap/asynctask/MainActivity.java
+++ app/src/main/java/br/com/fiap/asynctask/MainActivity.java
@@ -2,12 +2,18 @@ package br.com.fiap.asynctask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String IMG_URL = "https://www.douglascabral.com.br/aula-api/img/01.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void download(View view) {
     }
 }
